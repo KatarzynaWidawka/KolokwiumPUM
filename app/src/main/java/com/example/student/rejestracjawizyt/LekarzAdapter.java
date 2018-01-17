ffd2008a5973bde095f5e5a8a438a7c5d468fe24
unknown <student@IBLAB732P-20.ib.polsl.pl>
2018-01-17 14:45:45 +0100

    Gotowe wszystkie widoki

--- app/src/main/java/com/example/student/rejestracjawizyt/LekarzAdapter.java
+++ app/src/main/java/com/example/student/rejestracjawizyt/LekarzAdapter.java
@@ -1,8 +1,41 @@
 package com.example.student.rejestracjawizyt;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
 /**
  * Created by student on 2018-01-17.
  */
 
-public class LekarzAdapter {
+public class LekarzAdapter  extends RecyclerView.Adapter<LekarzAdapter.ViewHolder> {
+
+    ArrayList<LEKARZ> lekarze = new ArrayList<>();
+    LekarzAdapter(ArrayList<LEKARZ>lekarze){this.lekarze=lekarze;}
+
+    @Override
+    public LekarzAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list, null);
+        return new ViewHolder(itemLayoutView);
+    }
+
+    @Override
+    public void onBindViewHolder(LekarzAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lekarze.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
